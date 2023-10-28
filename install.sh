a0fe9c8158bf3c50c6e485faee102ecca98ebc50
Eero H??ih??l?? <eero.haihala@gmail.com>
2023-10-28 21:20:13 +0300

    Add installation script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -euo pipefail
+
+# This should fail if not in a git repo
+destination=`git rev-parse --git-dir`/hooks/shortcut-git-hook.sh
+hookDir=$(dirname $destination)
+mkdir -p $hookDir
+
+read -s -p "Give shortcut api token: " token
+
+# Just some request to test the token works. This should fail if the token was invalid
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -H "Shortcut-Token: $token" \
+  -L "https://api.app.shortcut.com/api/v3/categories"
+
+curl -Lo $destination https://raw.githubusercontent.com/haihala/shortcut-hook/main/shortcut-git-hook.sh
+chmod +x $destination
+sed -i "s/PUT-SHORTCUT-API-TOKEN-HERE/$token/g" $destination
+
+# This is done this way so that the existing gerrit hook is not bothered
+echo "exec $destination" >> "$hookDir/commit-msg"
+chmod +x $hookDir/commit-msg
+
+echo "Hook installed"
