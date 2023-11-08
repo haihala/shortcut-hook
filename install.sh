12eb5c05c67fcf78129a7c4de6ed4a80899956cf
Eero Häihälä <eero.haihala@vincit.fi>
2023-11-08 18:22:45 +0200

    Make install script token validation nicer looking

--- install.sh
+++ install.sh
@@ -9,14 +9,18 @@ mkdir -p $hookDir
 
 read -s -p "Give shortcut api token: " token
 
-# Just some request to test the token works. This should fail if the token was invalid
+echo
+echo "Testing token"
+
 curl -s -X GET \
     --fail \
     -H "Content-Type: application/json" \
     -H "Shortcut-Token: $token" \
-    -L "https://api.app.shortcut.com/api/v3/categories"
-# Output looks nicer if we put an empty line here
+    -L "https://api.app.shortcut.com/api/v3/categories" ||
+    (echo "Invalid token" && exit 1)
+
 echo
+echo "Token works"
 
 curl -Lo $destination https://raw.githubusercontent.com/haihala/shortcut-hook/main/shortcut-git-hook.sh
 chmod +x $destination
